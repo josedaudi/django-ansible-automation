d595f6b713fda076a85093f5cfe29d6d041a2c24
Joseph Daudi <josedaudi@gmail.com>
2022-08-17 16:23:09 +0300

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./deployer/deploy.yaml --private-key=./ssh_keys127.0.0.1_prod_key -K -u deployer -i ./deployer/hosts -vvv
+ansible-playbook ./deployer/deploy.yaml --private-key=/Users/josedaudi/MEGA/automation-demo.pem -K -u ubuntu -i ./deployer/hosts -vvv
